76acdb8a69b2cf8138ad63ca31649d03f5a46781
Bailey Borchert <baileyborchert@gmail.com>
2021-03-30 20:10:51 -0500

    Add styles for buttons

--- js/whitelist.js
+++ js/whitelist.js
@@ -14,7 +14,7 @@ function showTab(n) {
     if (n == (x.length - 1)) {
     document.getElementById("nextBtn").innerHTML = "Submit";
     } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").innerHTML = "Next »";
     }
     //... and run a function that will display the correct step indicator:
     fixStepIndicator(n)
@@ -24,7 +24,7 @@ function nextPrev(n) {
     // This function will figure out which tab to display
     var x = document.getElementsByClassName("tab");
     // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
+    //if (n == 1 && !validateForm()) return false;
     // Hide the current tab:
     x[currentTab].style.display = "none";
     // Increase or decrease the current tab by 1:
@@ -39,6 +39,7 @@ function nextPrev(n) {
     showTab(currentTab);
 }
 
+/*
 function validateForm() {
     // This function deals with validation of the form fields
     var x, y, i, valid = true;
@@ -59,7 +60,7 @@ function validateForm() {
     document.getElementsByClassName("step")[currentTab].className += " finish";
     }
     return valid; // return the valid status
-}
+} */
 
 function fixStepIndicator(n) {
     // This function removes the "active" class of all steps...
